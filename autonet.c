a56b6af3da06bf203009c971392756db13ad4eba
Steven Dee <steve@smartercode.net>
2015-02-12 12:17:44 -0500

    Print network filename

--- autonet.c
+++ autonet.c
@@ -134,6 +134,7 @@ int main(void)
 	}
 
 	if (found) {
+		printf("network found:%s\n", NetPref_filename(net_pref));
 		if (lstat(HOSTNAME_IF, &sb) < 0) {
 			if (errno != ENOENT)
 				err(1, "lstat");
